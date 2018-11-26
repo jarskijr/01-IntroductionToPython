7c5f4397dcef7dfb89caf2546a75180cbda88f5d
jarskijr <jarskijr@rose-hulman.edu>
2018-11-26 09:50:32 -0500

    Mission Accomplished

--- src/m4_SimpleTurtle_objects.py
+++ src/m4_SimpleTurtle_objects.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Jacob Jarski.
 """
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
 #   On Line 13 above, replace  PUT_YOUR_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -48,7 +48,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Run this module.  A window will pop up and Turtles will move around.
 #   After the Turtles stop moving,
 #      ** click anywhere in the window to close the window **.
@@ -104,7 +104,7 @@ boris.forward(200)
 #     set its  pen  and  speed  INSTANCE VARIABLES, and ask it to do things.
 # -----------------------------------------------------------------------------
 natasha = rg.SimpleTurtle('turtle')
-natasha.pen = rg.Pen('red', 30)  # Second argument is the Pen's thickness
+natasha.pen = rg.Pen('blue', 20)  # Second argument is the Pen's thickness
 natasha.speed = 5  # Bigger means faster, max is usually about 10
 
 natasha.backward(50)
@@ -112,11 +112,23 @@ natasha.right(90)
 natasha.forward(125)
 
 natasha.speed = 1  # Now slower
-natasha.go_to(rg.Point(-100, 200))
+natasha.go_to(rg.Point(300, -250))
+
+
+victor = rg.SimpleTurtle('turtle')
+victor.pen = rg.Pen('green', 10)
+victor.speed = 3
+
+victor.backward(100)
+victor.right(120)
+victor.forward(240)
+victor.draw_circle(40)
+# I'm not entirely sure what the natasha.write_text() command does, and I was curious how you would use it
+
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add a few more lines of your own code to make one of the existing
 #   SimpleTurtles move some more and/or have different characteristics.
 #
@@ -129,7 +141,7 @@ natasha.go_to(rg.Point(-100, 200))
 
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   The above code  CONSTRUCTS  two SimpleTurtle objects
 #   and gives those objects NAMES:
 #       boris    natasha
